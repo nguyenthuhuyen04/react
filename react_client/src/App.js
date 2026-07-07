1d422187aa6af0b5df9adca764cf74dc7bc886ae
nguyenthuhuyen04 <24100435@st.phenikaa-uni.edu.vn>
2026-07-07 08:21:27 +0000

    react-1

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -31,13 +31,13 @@ function App() {
     <div className="app-shell">
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">MY APP</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown title="Options" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
                   Another action
